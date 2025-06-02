7db6807656f0a58994b1f9af18932c8b785a95f5
narasimhaawsdev <110443565+narasimhaawsdev@users.noreply.github.com>
2025-06-02 11:46:16 +0530

    fix bug

--- Angular JS/my-app/src/app/app.component.ts
+++ Angular JS/my-app/src/app/app.component.ts
@@ -3,9 +3,8 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: './app.component.css'
 })
 export class AppComponent {
   title = 'my-app';
